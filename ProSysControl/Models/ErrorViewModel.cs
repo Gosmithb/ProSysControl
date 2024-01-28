47c43a6e9af80bf34fea981c19d109ba03c3530d
Gosmithb <bryangodinez00@gmail.com>
2024-01-28 13:33:48 -0800

    Primer commit

---
+++ ProSysControl/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProSysControl.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
